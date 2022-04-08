efc49d7499a9623022fc07ebffec341ce443bbab
Leila Soleimani <l.soleimani2000@gmail.com>
2022-04-08 14:50:57 +0430

    added new project for search with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import ResultList from './components/ResultList/ResultList';
+import SearchBox from './components/SearchBox/SearchBox';
+import useDebouncedCallback from './hooks/useDebouncedCallback';
+import useFetchData from './hooks/useFetchData';
+import useSelectedItem from './hooks/useSelectedItem';
+import Hotels from './models/Hotels';
 
 function App() {
+  const [term, setTerm]= useState<string>("");
+  const debouncedFetchData = useDebouncedCallback(getData);
+  const { data, fetchData, clearData } = useFetchData();
+  const {selectedItem, fetchSelected} = useSelectedItem();
+
+
+  useEffect(() => {
+    debouncedFetchData();
+  },[term]);
+
+  function getData():void {
+    if(!term) {
+     clearData();
+    } else {
+      fetchData(term);
+    }
+  }
+
+  const onSelectHotel = (hotel: Hotels) => {
+    fetchSelected(hotel);
+    // console.log(selectedItem);
+    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        search hotel
       </header>
+      <main>
+        <SearchBox onChange={(term) => setTerm(term)}/>
+        <ResultList data={data}  onSelectItem={onSelectHotel}/>
+      </main>
     </div>
   );
 }
